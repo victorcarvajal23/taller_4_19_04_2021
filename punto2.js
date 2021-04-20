92b9ded487998c9c28bab5cebf56694f83655f37
victorcarvajal23 <victorcarvajal016@gmail.com>
2021-04-20 12:18:50 -0500

    fin

--- punto2.js
+++ punto2.js
@@ -29,9 +29,12 @@ let conta=0
 let codigo
 let contenedor
 let contenedor2
+let max
+let max2=[]
 
 let precios=[400,1000,2200,1500,2500,2800,2000]
-let nombres=[" 1.Bonais"," 2.Palito de helado de agua"," 3.Palito de helado de crema"," 4.Bombón helado con arequipe"," 5.Bombón helado con chispas de chocolate"," 6.Bombón helado con fresas"," 7.Medio litro de helado"]
+let nombres=["Bonais","Palito de helado de agua","Palito de helado de crema","Bombón helado con arequipe","Bombón helado con chispas de chocolate","Bombón helado con fresas","Medio litro de helado"]
+//let nombres=[" 1.Bonais"," 2.Palito de helado de agua"," 3.Palito de helado de crema"," 4.Bombón helado con arequipe"," 5.Bombón helado con chispas de chocolate"," 6.Bombón helado con fresas"," 7.Medio litro de helado"]
 let codigoHelados=[1,2,3,4,5,6,7]
 while(inicio==true){
 dinero=parseInt(prompt("Ingrese la cantidad de dinero"))
@@ -57,17 +60,36 @@ while(inicio2==true){
 if ((cliente=="MIGUEL"&&dinero==1000) || (cliente=="CARLOS"&&dinero==2000)||( cliente=="MANUEL"&&dinero==500)) {
     for (let i = 0; i < precios.length; i++) {
         if (precios[i]<=dinero) {
-            conta++
             seleccion[conta]=nombres[i]
             valorF[conta]=precios[i]
             seleccion2[conta]=codigoHelados[i]
+            conta++
         }   
     }
     console.log("Dinero "+dinero)
     console.log("Cliente "+cliente)
+    //console.log(seleccion)
+    max=valorF[0]
+    for (let i = 0; i < valorF.length; i++) {
+        
+        if (valorF[i] >= max) {
+            max=valorF[i]
+            max2=seleccion[i]
+            
+        }   
+    }
+    //console.log("max"+valorF)
+    console.log(`pudo comprar(${seleccion})`)
+    let vueltos
+    vueltos=dinero-max
+    console.log(`compro un (${max2}) por un valor de ${max} y le sobro ${vueltos}`)
+
+
+
+
         //seleccionar helado
 
-    while(inicio3==true){
+/*     while(inicio3==true){
 
     codigo=parseInt( prompt(`Puede comprar \n${seleccion}\nIngrese el codigo del producto que desea comprar`))
     let valida2=Number.isInteger(codigo)
@@ -93,7 +115,7 @@ if ((cliente=="MIGUEL"&&dinero==1000) || (cliente=="CARLOS"&&dinero==2000)||( cl
         alert("ingrese un codigo valido")
         inicio3=true
         }
-    }
+    } */
 }
 else{
     alert("Ese usurio no tiene ese dinero")
